9354e9ee0f66db982d3bee3283db4674ae567b6d
Diego Rea Morales <diegoream23@gmail.com>
2025-10-09 20:11:20 -0600

    19 formateada

--- Practica19/script.js
+++ Practica19/script.js
@@ -1,29 +1,19 @@
 function dibujarPoligono() {  
   const canvas = document.getElementById("canvas");
-
   const lados = document.getElementById("lados").value;
-
   const ctx = canvas.getContext("2d");
-
   const centroX = canvas.width / 2;
-
   const centroY = canvas.height / 2;
-
   const radio = 70;
-
   const angulo = (2 * Math.PI) / lados;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
   ctx.beginPath();
-
   ctx.moveTo(centroX + radio * Math.cos(0), centroY + radio * Math.sin(0));
 
   for (let i = 1; i <= lados; i++) {
     const x = centroX + radio * Math.cos(i * angulo);
-
     const y = centroY + radio * Math.sin(i * angulo);
-
     ctx.lineTo(x, y);
   }
   let angulos = document.getElementById("angle");
@@ -31,11 +21,9 @@ function dibujarPoligono() {
     lados == 0 ? "" : "Valor de los Angulos: " + (180 * (lados - 2)) / lados;
 
   ctx.closePath();
-
   ctx.stroke();
 }
 function dibujarRecta() {
-
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
 
@@ -47,10 +35,8 @@ function dibujarRecta() {
   const fin = centroX + (recta / 2);
 
   ctx.beginPath();
-
   ctx.moveTo(inicio, centroY);
   ctx.lineTo(fin, centroY);
-
   ctx.closePath();
   ctx.stroke();
 }
