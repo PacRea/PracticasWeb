30f94eb9824bb41ac3005b467c2c931d77764330
Diego Rea Morales <diegoream23@gmail.com>
2025-11-06 17:28:47 -0600

    corregidos ciertos aspectos

---
+++ Practica23/calculadora/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");
+  const handleClick = (value) => {
+    if (value === "=") {
+      try {
+        const calculatedResult = eval(expression);
+        setResult(calculatedResult);
+      } catch (error) {
+        setResult("Error");
+      }
+    } else if (value === "C") {
+      setExpression("");
+      setResult("");
+    } else {
+      setExpression((prevExpression) => prevExpression + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={expression} readOnly />
+        <input type="text" value={result} readOnly />
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
+        <button onClick={() => handleClick("+")}>+</button>
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+        <button onClick={() => handleClick("-")}>-</button>
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button onClick={() => handleClick("*")}>*</button>
+        <button onClick={() => handleClick("0")}>0</button>
+        <button onClick={() => handleClick(".")}>.</button>
+        <button onClick={() => handleClick("=")}>=</button>
+        <button onClick={() => handleClick("/")}>/</button>
+        <button onClick={() => handleClick("C")}>C</button>
+        <button onClick={() => handleClick("**2")}>^2</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
